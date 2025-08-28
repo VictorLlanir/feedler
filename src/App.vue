3f5099fc80f02b18f47deefedc7bf5e26ec44a97
Victor Llanir <victortrevisan1997@gmail.com>
2025-08-27 21:56:14 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+  count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+  console.log(`The initial count is ${count.value}.`)
+})
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+
+  <button @click="increment">Count is: {{ count }}</button>
+</template>
